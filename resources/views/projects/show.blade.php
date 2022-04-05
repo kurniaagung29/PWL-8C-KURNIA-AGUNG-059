d9442484af8a3ee026c626b2157f718e40831f89
kurniaagung29 <kurnia.kharisma@student.unmer.ac.id>
2022-04-05 12:02:49 +0700

    fisrt commit

---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> {{ $project->name }}</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('projects.index') }}" title="Go back"> <i
+                        class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                {{ $project->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kurir:</strong>
+                {{ $project->introduction }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                {{ $project->location }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Biaya:</strong>
+                {{ $project->cost }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tanggal Dibuat:</strong>
+                {{ date_format($project->created_at, 'jS M Y') }}
+            </div>
+        </div>
+    </div>
+@endsection
